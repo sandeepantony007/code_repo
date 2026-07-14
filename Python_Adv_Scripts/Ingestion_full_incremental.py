495b254dc5db07d1fc86157b84c3335ec2495589
sandeepantony007 <sandeep.antony007@gmail.com>
2026-07-13 22:39:45 -0500

    Updated API Ingestion
    Updated API Ingestion script with Sample output details

--- Python_Adv_Scripts/Ingestion_full_incremental.py
+++ Python_Adv_Scripts/Ingestion_full_incremental.py
@@ -141,3 +141,29 @@ if __name__ == "__main__":
 
     for book in new_books:
         print(book)
+
+
+"""
+Sample Output:
+Total books loaded: 10
+Watermark (Latest Publish Year): 2021
+
+Books from Full Load
+
+{'title': 'Python in a Nutshell', 'author': 'Alex Martelli, Anna Ravenscroft, Steve Holden', 'first_publish_year': 2002, 'rating': 3.0}
+{'title': 'Python Programming', 'author': 'Reema Thareja', 'first_publish_year': 2019, 'rating': 'N/A'}
+{'title': 'Starting out with Python', 'author': 'Tony Gaddis', 'first_publish_year': 2008, 'rating': 'N/A'}
+{'title': 'Deep Learning with Python', 'author': 'Francois Chollet, Matthew Watson', 'first_publish_year': 2017, 'rating': 3.0}
+{'title': 'The Python Project', 'author': 'Victor Canning', 'first_publish_year': 1967, 'rating': 'N/A'}
+{'title': 'Python for Kids', 'author': 'Jason R. Briggs', 'first_publish_year': 2012, 'rating': 5.0}
+{'title': 'PYTHON Exercises', 'author': 'Ray Yao', 'first_publish_year': 2021, 'rating': 'N/A'}
+{'title': 'Python programming', 'author': 'John M. Zelle', 'first_publish_year': 2003, 'rating': 'N/A'}
+{'title': 'Michael Palin Diaries 1969-1979', 'author': 'Michael Palin', 'first_publish_year': 2006, 'rating': 3.5}
+{'title': 'Python Data Science Handbook', 'author': 'Jake VanderPlas', 'first_publish_year': 2016, 'rating': 4.0}
+
+Running Incremental Load...
+
+
+Reading Incremental Load - Page 5
+New Books Found: 1
+"""
